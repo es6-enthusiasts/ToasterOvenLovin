45482f5d17b04814f8d0f79b4373d3d30dd27886
willjsimmons <willjsimmons@gmail.com>
2023-11-14 11:26:06 -1000

    fixed object issue error.

--- app/imports/ui/components/RecipeGrid.jsx
+++ app/imports/ui/components/RecipeGrid.jsx
@@ -4,6 +4,7 @@ import { useTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 import RecipeCard from './Recipe';
 import { Recipes } from '../../api/stuff/Recipes';
+import LoadingSpinner from './LoadingSpinner';
 
 const RecipeGrid = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
@@ -21,6 +22,9 @@ const RecipeGrid = () => {
       ready: rdy,
     };
   }, []);
+  console.log(ready);
+  console.log(recipes);
+
   return (ready ? (
     <Container className="py-3">
       <Row className="justify-content-center">
@@ -31,7 +35,7 @@ const RecipeGrid = () => {
         ))}
       </Row>
     </Container>
-  ) : {});
+  ) : <LoadingSpinner />);
 };
 
 export default RecipeGrid;
