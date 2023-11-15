f62f0757acc171ba3f3567fd88189567a1f4318a
willjsimmons <willjsimmons@gmail.com>
2023-11-15 11:10:07 -1000

    linked listVendors to TopMenu

--- app/imports/ui/components/TopMenu.jsx
+++ app/imports/ui/components/TopMenu.jsx
@@ -18,7 +18,7 @@ const TopMenu = () => {
         <Nav className="mx-auto pt-auto">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#recipes">Recipes</Nav.Link>
-          <Nav.Link href="#vendors">Vendors</Nav.Link>
+          <Nav.Link href="/listVendors">Vendors</Nav.Link>
           <Nav.Link href="#community">Community</Nav.Link>
           <Nav.Link href="#categories">Categories</Nav.Link>
         </Nav>
