01372eed17a0f9c547b27c0969aac5e2b3d8673b
willjsimmons <willjsimmons@gmail.com>
2023-11-17 12:16:55 -1000

    Top Menu Variation implemented. we dont yet have roles for it

--- app/imports/ui/components/TopMenu.jsx
+++ app/imports/ui/components/TopMenu.jsx
@@ -5,24 +5,67 @@ import { Navbar, Nav, Container, NavDropdown, Image } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import { BoxArrowRight, PersonFill, PersonPlusFill } from 'react-bootstrap-icons';
 
-const TopMenu = () => {
-
-  const { currentUser } = useTracker(() => ({
-    currentUser: Meteor.user() ? Meteor.user().username : '',
-  }), []);
-
-  return (
-    <Navbar className="color5" expand="sm">
-      <Container>
-        <Navbar.Brand href="/home"><Image className="bannerLogo" src="images/logo.png" alt="logo" /><strong>College Cuisine Connection</strong></Navbar.Brand>
+const navContent = function (User, role) {
+  let retVal;
+  if (User === '') {
+    retVal = (
+      <Nav className="mx-auto pt-auto">
+        <Nav.Link href="/home">Home</Nav.Link>
+        <Nav.Link href="/listRecipes">Recipes</Nav.Link>
+        <Nav.Link href="/listVendors">Vendors</Nav.Link>
+        <Nav.Link href="/community">Community</Nav.Link>
+      </Nav>
+    );
+  } else {
+    if (role === 'Admin') {
+      retVal = (
         <Nav className="mx-auto pt-auto">
           <Nav.Link href="/home">Home</Nav.Link>
           <Nav.Link href="/listRecipes">Recipes</Nav.Link>
           <Nav.Link href="/listVendors">Vendors</Nav.Link>
           <Nav.Link href="/listIngredients">Ingredients</Nav.Link>
           <Nav.Link href="/community">Community</Nav.Link>
-
         </Nav>
+      );
+    } if (role === 'Vendor') {
+      retVal = (
+        <Nav className="mx-auto pt-auto">
+          <Nav.Link href="/home">Home</Nav.Link>
+          <Nav.Link href="/listRecipes">Recipes</Nav.Link>
+          <Nav.Link href="/listVendors">Vendors</Nav.Link>
+          <Nav.Link href="/listIngredients">Ingredients</Nav.Link>
+          <Nav.Link href="/community">Community</Nav.Link>
+        </Nav>
+      );
+    }
+    retVal = (
+      <Nav className="mx-auto pt-auto">
+        <Nav.Link href="/home">Home</Nav.Link>
+        <Nav.Link href="/listRecipes">Recipes</Nav.Link>
+        <Nav.Link href="/listVendors">Vendors</Nav.Link>
+        <Nav.Link href="/listVendors">MyCookbook</Nav.Link>
+        <Nav.Link href="/listIngredients">Ingredients</Nav.Link>
+        <Nav.Link href="/community">Community</Nav.Link>
+      </Nav>
+    );
+  }
+  return retVal;
+
+};
+
+// Use navContent in your component
+const TopMenu = () => {
+
+  const { currentUser, role } = useTracker(() => ({
+    currentUser: Meteor.user() ? Meteor.user().username : '',
+    role: 'Admin',
+  }), []);
+
+  return (
+    <Navbar className="color5" expand="sm">
+      <Container>
+        <Navbar.Brand href="/home"><Image className="bannerLogo" src="images/logo.png" alt="logo" /><strong>College Cuisine Connection</strong></Navbar.Brand>
+        {navContent(currentUser, role)}
         <Nav className="justify-content-end">
           {currentUser === '' ? (
             <NavDropdown id="login-dropdown" title="Login">
