b5b751176f35bc48f5b56ed0ec009b7cfe98972b
Kyla Lee <112616409+kyla8@users.noreply.github.com>
2023-11-16 20:11:04 -1000

    having errors importing the account

--- app/imports/ui/pages/Community.jsx
+++ app/imports/ui/pages/Community.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import Banner from '../components/Banner';
 import RecipeGrid from '../components/RecipeGrid';
+import Accounts from '../server/Accounts.js';
 
 /* A simple static component to render some text for the landing page. */
 const Community = () => (
   <Container id="landing-page" fluid className="py-0">
     <Banner />
     <Container className="content color1 my-2">
-      <Row className="text-center">
-        <h1> Featured Recipes </h1>
+      <Row xs={1} md={2} lg={3} className="g-4">
+        {Accounts.map((username) => (<Col key={username}><Accounts username={username} /></Col>))}
       </Row>
     </Container>
     <RecipeGrid />
