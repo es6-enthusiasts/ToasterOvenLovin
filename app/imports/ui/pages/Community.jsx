3132970df08ac2b7cabba1cf486b13d49c5ec2a8
willjsimmons <willjsimmons@gmail.com>
2023-11-23 12:30:25 -1000

    Community Page is fully functional. Fixed sign in recipes issue.

--- app/imports/ui/pages/Community.jsx
+++ app/imports/ui/pages/Community.jsx
@@ -2,12 +2,10 @@ import { Col, Container, Row } from 'react-bootstrap';
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
-import PropTypes from 'prop-types';
 import Banner from '../components/Banner';
 import AccountCard from '../components/Account';
-import { Recipes } from '../../api/recipe/Recipes';
-
 import LoadingSpinner from '../components/LoadingSpinner';
+import { Profiles } from '../../api/profiles/Profiles';
 
 /* A simple static component to render some text for the landing page. */
 const Community = () => {
@@ -16,50 +14,13 @@ const Community = () => {
   // Note that this subscription will get cleaned up
   // when your component is unmounted or deps change.
   // Get access to Stuff documents.
-    const subscription = Meteor.subscribe(Recipes.generalPublicationName);
-    const sample = [
-      {
-        firstName: 'William',
-        lastName: 'Simmons',
-        address: '1234 candyCane Lane',
-        image: 'https://github.com/willjsimmons.png',
-        description: 'an adept leader who succeeds everywhere all the time.',
-      },
-      {
-        firstName: 'Juliette',
-        lastName: 'Raubolt',
-        address: '1234 candyCane Lane',
-        image: 'https://github.com/julietteraubolt.png',
-        description: 'an adept leader who succeeds everywhere all the time.',
-      },
-      {
-        firstName: 'Loelle',
-        lastName: 'Lam',
-        address: '1234 candyCane Lane',
-        image: 'https://github.com/loellelam.png',
-        description: 'an adept leader who succeeds everywhere all the time.',
-      },
-      {
-        firstName: 'Janel',
-        lastName: 'Joson',
-        address: '1234 candyCane Lane',
-        image: 'https://github.com/janeljo.png',
-        description: 'an adept leader who succeeds everywhere all the time.',
-      },
-      {
-        firstName: 'Kyla',
-        lastName: 'Lee',
-        address: '1234 candyCane Lane',
-        image: 'https://github.com/kyla8.png',
-        description: 'an adept leader who succeeds everywhere all the time.',
-      },
-    ];
+    const subscription = Meteor.subscribe(Profiles.generalPublicationName);
     // Determine if the subscription is ready
     const rdy = subscription.ready();
     // Get the Stuff documents
-    const accountItems = Recipes.collection.find({}).fetch();
+    const accountItems = Profiles.collection.find({}).fetch();
     return {
-      accounts: sample,
+      accounts: accountItems,
       ready: rdy,
     };
   }, []);
