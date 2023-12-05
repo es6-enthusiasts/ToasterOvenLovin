1daec10da11b59590b16831e399fddb9473bf4ca
loellelam <105581432+loellelam@users.noreply.github.com>
2023-12-04 21:48:50 -1000

    Link to tutorial page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Button, Container, Row } from 'react-bootstrap';
 // import Banner from '../components/Banner';
 import RandomRecipes from '../components/RandomRecipes';
 import InfoCarousel from '../components/InfoCarousel';
+import { Link } from 'react-router-dom';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
@@ -15,6 +16,13 @@ const Landing = () => (
       </Row>
     </Container>
     <RandomRecipes />
+    <Container className="content color1 my-2 p-2">
+      <Row className="text-center">
+        <h2>
+          <a href="https://es6-enthusiasts.github.io/CollegeCuisineConnect/tutorial">Learn to Get Started!</a>
+        </h2>
+      </Row>
+    </Container>
   </Container>
 );
 
