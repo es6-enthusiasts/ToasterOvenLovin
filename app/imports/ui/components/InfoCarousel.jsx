fdbd40886a83fc104b7d3948ac8d74630c7296af
Janel Michaela R Joson <120702337+janeljo@users.noreply.github.com>
2023-12-04 18:42:01 -1000

    Update Landing page style

--- app/imports/ui/components/InfoCarousel.jsx
+++ app/imports/ui/components/InfoCarousel.jsx
@@ -6,9 +6,9 @@ const InfoCarousel = () => (
     <Carousel.Item className=" slide1 fullBg">
       <Col className="slantRight accent1" md={4}>
         <div>
-          <Carousel.Caption className="unslant">
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <Carousel.Caption className="unslant text">
+            <h3>Student-Friendly Feasts</h3>
+            <p>Effortless recipes, maximum flavor satisfaction.</p>
           </Carousel.Caption>
         </div>
       </Col>
@@ -16,9 +16,9 @@ const InfoCarousel = () => (
     <Carousel.Item className=" slide2 fullBg">
       <Col className="slantRight accent2" md={4}>
         <div>
-          <Carousel.Caption className="unslant">
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <Carousel.Caption className="unslant text">
+            <h3>Quick and Tasty</h3>
+            <p>Easy recipes for busy days and hungry nights.</p>
           </Carousel.Caption>
         </div>
       </Col>
@@ -26,9 +26,9 @@ const InfoCarousel = () => (
     <Carousel.Item className=" slide3 fullBg">
       <Col className="slantRight accent3" md={4}>
         <div>
-          <Carousel.Caption className="unslant">
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <Carousel.Caption className="unslant text">
+            <h3>Meals Made Easy</h3>
+            <p>Turn simple ingredients into culinary delights.</p>
           </Carousel.Caption>
         </div>
       </Col>
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
-import Banner from '../components/Banner';
+// import Banner from '../components/Banner';
 import RandomRecipes from '../components/RandomRecipes';
 import InfoCarousel from '../components/InfoCarousel';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <Container id="landing-page" fluid className="py-0">
-    <InfoCarousel />
-    <Banner />
+    <InfoCarousel className="pb-5" />
+    {/* <Banner /> */}
     <Container className="content color1 my-2">
       <Row className="text-center">
         <h2> Featured Recipes </h2>
